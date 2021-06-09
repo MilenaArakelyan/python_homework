d0d0a64d8986ade96f3912f6f9a3a8db515ee663
MilenaArakelyan <milenaarakelyan21@gmail.com>
2021-06-10 00:55:53 +0400

    any text

---
+++ lesson4.py
@@ -0,0 +1,24 @@
+class People:
+    def _init_(self,name,last_name,age):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+    
+    def add_age(self,age):
+        self.age += age
+        return self.age
+    
+    def __repr__(self):
+        return self.name
+        
+class Worker(People):
+    def _init_ (self,name,last_name,age,workplace):
+        super()._init_(name, last_name, age)  
+        
+        
+worker = Worker ("Victor","Hambardzumyan",100,"atronom")
+print(worker.add_age(3))
+print (worker.age)
+
+
+
